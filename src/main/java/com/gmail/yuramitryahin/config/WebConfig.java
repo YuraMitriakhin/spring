99116864c6b4876ef8fc50e4e6bb07ffa6676005
Yura <yuramitryahin@gmail.com>
2021-02-11 14:10:54 +0200

    fixed bugs

--- src/main/java/com/gmail/yuramitryahin/config/WebConfig.java
+++ src/main/java/com/gmail/yuramitryahin/config/WebConfig.java
@@ -1,22 +1,11 @@
 package com.gmail.yuramitryahin.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.gmail.yuramitryahin.controller")
 public class WebConfig {
-    @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/META-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
